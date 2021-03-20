ecb8f71a9ff46644ea5ffd2014bdf7b219f8b880
robert morales <rmoralfit@gmail.com>
2021-03-20 12:32:57 -0400

    done

---
+++ index.js
@@ -0,0 +1,15 @@
+function divide () {
+    return divide;
+}
+function divide() {
+    return 2000 / 100;
+}
+ 
+square = x => x * x;
+
+function add() {
+    return add;
+}
+function add(a, b) {
+    return a + b;
+}
